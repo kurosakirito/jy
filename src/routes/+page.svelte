498c4bd89b07c8415b6c94e9bca8a55659c3d7f0
Henry Leung Kwan Wong <henry.lk.wong@asl.Local>
2023-12-27 11:12:07 +0800

    inital

---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+    let books = ["seDiuYingHungChyun", "sanDiuHapLeui", "yiTinTouLungGei"];
+</script>
+
+<style>
+    .center {
+        text-align:center;
+    }
+    .tableCenter {
+        margin: auto;
+    }
+</style>
+
+
+<h2 class="center">Books</h2>
+
+<table class="tableCenter">
+    {#each books as book}
+        <tr>
+            <td>{book}</td>
+        </tr>
+    {/each}
+</table>
+
